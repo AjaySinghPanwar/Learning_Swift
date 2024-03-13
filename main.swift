2b37d0f091be9d7e4004d985b8a55b4283cd5061
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-03-13 12:13:42 +0530

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,2 +1,28 @@
 // How to print something to screen -> And this is how we add comments in Swift using "//"
 print("Hi, this is the first line I have written in Swift")
+
+// Swift variables, constants and literals
+// Use let for declaring a constant and var for declaring a variable
+let score1 = 23
+// Uncomment and run below line and see the error you got
+// score1 = 24 
+
+var score2 = 100
+print(score2)
+
+let score3: Int
+score3 = 67
+
+// You can also give type to a variable
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+let explicitFloat: Float = 2
+
+// Swift literals
+// Integer literal
+// Floating-point literal
+// Boolean literal
+// String and Character literal
+var stringLiteral = "This is the string literal"
+var characterLiteral = "A"
